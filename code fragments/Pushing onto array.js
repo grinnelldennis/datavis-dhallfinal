66f380da342bf684cbbbca66485389228babb376
grinnelldennis <chanchun@grinnell.edu>
2017-04-26 17:03:55 -0500

    handling single day totals by making new array

--- code fragments/Pushing onto array.js
+++ code fragments/Pushing onto array.js
@@ -1,29 +1,38 @@
 // Some array to hold all data
 var someArray = {}; 
+var dayTotalArray = {};
 
 // Populating something with every row in csv sheets 
 for (var row in 2015spring) {
-	// pre-process time information
-	// time conversion
-	var time = row.TimeIn;
-	var apm = time.substring(6,7);
-	var hour = parseInt(time.substring(0, 2));
-	hour = (ampm === "P")? hour+=12 : hour;
+	if (row.dash === "Day Totals:") {
+		// pushing single day totals onto separate array
+		dayTotalArray.push ({
+			date: new Date(row.Date),
+			day: row.Day,
+			week: row.Week,
+			dineIn: row.DineIn;
+			dineOut: row.Dine;
+		})
+	} else {
+		// pre-process time information
+		var time = row.TimeIn;
+		var apm = time.substring(6,7);
+		var hour = parseInt(time.substring(0, 2));
+		hour = (ampm === "P")? hour+=12 : hour;
 
-	// setting time
-	var date = new Date(row.Date);
-	date.setHours(hour);
-	date.setMinutes(time.substring(3, 5));
+		// writing time into a Date object
+		var date = new Date(row.Date);
+		date.setHours(hour);
+		date.setMinutes(time.substring(3, 5));
 
-	// popuating row onto object
-	someArray.push({
-		timeStamp: date,
-		dineIn: row.DineIn;
-		dineOut: row.Dine;
-	})	
+		// popuating row onto object
+		someArray.push({
+			timeStamp: date,
+			dineIn: row.DineIn;
+			dineOut: row.Dine;
+		})
+	}
 }
-
-
 
 
 // --TESTING--
