6e800b7232630632d00646c7559093cdf7609317
Benjamin Wong <wongbenj@grinnell.edu>
2017-05-07 20:51:36 -0500

    Updated plotted-stacked-bar with master

--- dhall.js
+++ dhall.js
@@ -1,15 +1,33 @@
+Skip to content
+This repository
+Search
+Pull requests
+Issues
+Gist
+ @wongbenj
+ Sign out
+ Watch 1
+  Star 0
+ Fork 0 grinnelldennis/datavis-dhallfinal
+ Code  Issues 0  Pull requests 0  Projects 0  Wiki  Pulse  Graphs
+Branch: master Find file Copy pathdatavis-dhallfinal/dhall.js
+e12a9ab  12 minutes ago
+@grinnelldennis grinnelldennis function version
+2 contributors @grinnelldennis @wongbenj
+RawBlameHistory     
+557 lines (471 sloc)  16.1 KB
 //------------------------------------------------------------------------
 // Global filtering options
-var diningin = false;
-var togo = false;
+var dnin = true;
+var togo = true;
 var weekSelector = 0;
 var wkDaySelected = [true, true, true, true, true, true, true];
 var semSelector = 0;
-var semester = [["06/01/2014", "06/01/2016"],
-          ["06/01/2014", "01/01/2015"],
-          ["01/01/2015", "06/01/2015"],
-          ["06/01/2015", "01/01/2016"],
-          ["01/01/2016", "06/01/2016"]];
+var semester = [["08/01/2014", "06/01/2016"],
+          ["08/01/2014", "12/20/2014"],
+          ["01/15/2015", "06/01/2015"],
+          ["08/01/2015", "12/20/2015"],
+          ["01/15/2016", "06/01/2016"]];
 
 
 //------------------------------------------------------------------------
@@ -18,20 +36,21 @@ var semester = [["06/01/2014", "06/01/2016"],
 // #Dining-in checkboxes
 d3.select('#diningin')
   .on('change', function() {
-	console.log(d3.select(this).node().checked); 
-	diningin = d3.select(this).node().checked;
-	updateStack();
-	updateLine();
-	});
+  console.log(d3.select(this).node().checked); 
+  diningin = d3.select(this).node().checked;
+  updateStack();
+  updateLine();
+  });
 
 // #To-Go Box checkboxes
 d3.select('#togo')
   .on('change', function() {
-	console.log(d3.select(this).node().checked); 
-	togo = d3.select(this).node().checked;
-	updateStack();
-	updateLine();
-	});
+  console.log(d3.select(this).node().checked); 
+  togo = d3.select(this).node().checked;
+  updateStack();
+  updateLine();
+  updateSemester();
+  });
 
 //---Drop-Down Handlers
 // Semester drop down
@@ -41,15 +60,17 @@ d3.select('#semester')
     semSelector = d3.select(this).node().value;
     updateStack();
     updateLine();
+    updateSemester();
   });
-	
+  
 // Week drop down
 d3.select('#week')
   .on('change', function() {
     console.log(d3.select(this).node().value);
     weekSelector = d3.select(this).node().value;
     updateStack();
-	  updateLine();
+    updateLine();
+    updateSemester();
   });
 
 // Day of week drop down
@@ -59,10 +80,11 @@ d3.select('#day')
     var daySelector = d3.select(this).node().value;
     wkDaySelected = [false, false, false, false, false, false, false];
     if (daySelector == 0) {
-		  wkDaySelected = [true, true, true, true, true, true, true];
-    }	else { wkDaySelected[daySelector] = true; }
+      wkDaySelected = [true, true, true, true, true, true, true];
+    } else { wkDaySelected[daySelector] = true; }
     updateStack();
-  	updateLine();
+    updateLine();
+    updateSemester();
 });
 
 
@@ -82,6 +104,10 @@ function updateLine() {
   displayLineChartSvg();
 }
 
+function updateSemester() {
+  populateSemesterArray(trafficByDay, getDate(0),  getDate(1));
+  displaySemseterLineGraph();
+}
 
 //------------------------------------------------------------------------
 //---CSV Loading  (Dennis)
@@ -93,15 +119,16 @@ d3.queue()
   .defer(d3.csv, 'formatted csv/2015s.csv')
   .defer(d3.csv, 'formatted csv/2016s.csv')
   .await(function(error, f2014, f2015, s2015, s2016) {
-    /* DATA PROCESSING */
+    // data processing
     processCsvData(f2014);
-    processCsvData(f2015);
     processCsvData(s2015);
+    processCsvData(f2015);
     processCsvData(s2016);
 
     // display default graphs
     updateStack();
     updateLine();
+    updateSemester();
 })
 
 // Array Objects Holding Aggregated Data
@@ -130,7 +157,7 @@ function processCsvData (data) {
       d.setUTCHours(hour);
       d.setUTCMinutes(+t.substring(j+1, j+3));
       // populates row into object
-      trafficByFifteen.push({ Date: d, DineIn: row.DineIn,  DineOut: row.DineOut });
+      trafficByFifteen.push({ Date: d, DineIn: row.DineIn,  DineOut: row.DineOut, Week: row.Week });
     }
   }
 }
@@ -142,7 +169,6 @@ function processCsvData (data) {
 // Data Array for Week Stack Plot
 var weeklyData = [];
 function populateWeekArray (a, d1, d3) {
-  console.log("d1, "+ d1 +" ;d2, "+ d3 +" ;wk, "+ weekSelector +" ;sem, "+semSelector);
   weeklyData = new Array;
   var max = 0;
   // initializes empty array
@@ -157,7 +183,6 @@ function populateWeekArray (a, d1, d3) {
       weeklyData[+row.Day].DineIn += +row.DineIn; 
       weeklyData[+row.Day].DineOut += +row.DineOut; 
       weeklyData[+row.Day].Count++;
-      if (+row.Day == 6) {console.log(row);}
     } 
   }
   // finds maximum
@@ -179,11 +204,13 @@ function populateDayArray (a, d1, d3) {
     dailyData.push({ Time: (Math.floor(i/4)+7) + ":" + ((i%4)*15),
                   DineIn: 0, DineOut: 0, Count: 0, AvgIn: 0, AvgOut: 0}); }
   // filter option
-  console.log(a.length);
+
+  if (weekSelector != 0) 
+    a = a.filter(function(d) { return +weekSelector <= +d.Week && +d.Week < +weekSelector+1; });
   a = a.filter(function(d) { return +d1 <= +d.Date && +d.Date <= +d3; });
-  console.log(a.length);
+
+
   //a = a.filter(function(d) {return wkDaySelected[+d.Day];})
-  console.log(a.length);
   for (var row of a) {
     var index = getArrayIndex(row.Date);
     if (0 <= index && index < 52){
@@ -206,6 +233,110 @@ function getArrayIndex (d) {
   return (d.getUTCHours()-7)*4 + (d.getUTCMinutes()-1)/15;
 }
 
+// Semester, Day-by-Day array 
+var semesterData = [];
+function populateSemesterArray(a, d1, d3) {
+  semesterData = new Array;
+  semesterData = a.filter(function (d) { return +d1 <= +d.Date && +d.Date <= +d3; })
+  if (weekSelector != 0) 
+    semesterData = semesterData.filter(function(d) { return +weekSelector <= +d.Week && +d.Week < +weekSelector+1; });
+  semesterData = semesterData.filter(function(d) {return wkDaySelected[+d.Day];})
+}
+
+
+//------------------------------------------------------------------------
+//---Zoom Chart over Day Array
+//---https://bl.ocks.org/mbostock/431a331294d2b5ddd33f947cf4c81319
+
+function displaySemseterLineGraph() {
+  var width_semeseter = plot_width*2;
+  // Set-up scales for stacked bar chart
+  var xScale = d3.scaleTime()
+    .domain([semesterData[0].Date, semesterData[semesterData.length-1].Date])
+    .range([0, width_semeseter], 0.05);
+
+  var yScale = d3.scaleLinear()
+    .domain([0, 3000])
+    .range([plot_height, 0]);
+
+  // Create easy colors accessible from the 10-step ordinal scale
+  var colors = d3.scaleOrdinal(d3.schemeCategory10);
+
+  // Create SVG for stacked bar chart
+  var svg_day = d3.select('body')
+    .append('svg')
+    .attr('width', svg_width*2)
+    .attr('height', svg_height);
+
+  // Add a group for each row of data
+  var groups = svg_day.selectAll('h')
+    .data(semesterData)
+    .enter()
+    .append('g')
+    .style('fill', function(d, i) {
+      return colors(i);
+    });
+
+  // Add a rect for each data value
+  var line_in = d3.line()
+    .x(function(d, i) { return xScale(d.Date)+margin*2; })
+    .y(function(d) { return yScale(d.DineIn)+margin; })
+    .curve(d3.curveCardinal.tension(.5));
+
+  var line_out = d3.line()
+    .x(function(d, i) { return xScale(d.Date)+margin*2; })
+    .y(function(d) { return yScale(d.DineOut)+margin; })
+    .curve(d3.curveLinear);
+
+  // x-axis title
+  svg_day.append('text')
+      .attr('class', 'x-axis')
+      .attr('x', plot_width)
+      .attr('y', 2 * margin + plot_height + label_height)
+      .text('Time During Day');
+
+  // Add the rotated y-axis title
+  svg_day.append('text')
+    .attr('class', 'y-axis')
+    .attr('text-anchor', 'middle')
+    .attr('transform',
+        'translate(' + margin/3 + ', ' + (plot_height / 2 + margin) + ')' + 
+        'rotate(-90)')
+    .text('Number of Swipes');
+
+  // Create x-axis and y-axis
+  var xaxis = d3.axisBottom(xScale);
+  var yaxis = d3.axisLeft(yScale);
+
+
+  svg_day.append('g')
+      .attr('transform', 'translate(' + 2 * margin + ', ' + (plot_height + margin) + ')')
+      .call(xaxis);
+
+  svg_day.append('g')
+      .attr('transform', 'translate(' + 2 * margin + ', ' + margin + ')')
+      .call(yaxis);
+
+  if (dnin)
+  svg_day.append("path")
+      .datum(semesterData)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("stroke-width", 1.5)
+      .attr("d", line_in);
+
+  if (togo)
+  svg_day.append("path")
+      .datum(semesterData)
+      .attr("fill", "none")
+      .attr("stroke", "orange")
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("stroke-width", 1.5)
+      .attr("d", line_out);
+}
 
 //------------------------------------------------------------------------
 //---SVG Drawing, Line 
@@ -300,7 +431,7 @@ function displayLineChartSvg() {
     .text('Number of Swipes');
 
   // Create x-axis and y-axis
-  var xaxis = d3.axisBottom(xScale);
+  var xaxis = d3.axisBottom(xScale).tickValues([]);
   var yaxis = d3.axisLeft(yScale);
 
 
@@ -327,7 +458,7 @@ var plot_width = svg_width - 5/2 * margin;
 var plot_height = svg_height - 5/2 * margin;
 
 // Sizing and spacing for plot components
-var label_height = 12 // Does not change font size, just an estimate
+var label_height = 12; // Does not change font size, just an estimate
 var label_spacing = 16;
 
 // Build an array of days of week
@@ -341,8 +472,8 @@ var daysOfWeek = [
     'Saturday'
 ];
 
+
 function displayStackedBar() {
-	
   // Set-up for stacked bar chart
   var stack = d3.stack()
     .keys(['AvgIn', 'AvgOut']);
@@ -429,7 +560,7 @@ function displayStackedBar() {
     .text('Number of Swipes');
 
   // Create x-axis and y-axis
-  var xaxis = d3.axisBottom(xScale);
+  var xaxis = d3.axisBottom(xScale).tickValues([]);
   var yaxis = d3.axisLeft(yScale);
 
 
@@ -441,36 +572,5 @@ function displayStackedBar() {
       .attr('transform', 'translate(' + 2 * margin + ', ' + margin + ')')
       .call(yaxis);
 }
-
-/*
-// Line Graph dining hall traffic over time
-var svg_height_2 = 800;
-var svg_width_2 = 800;
-var margin_2 = 100;
-var plot_width_2 = svg_width - 2 * margin;
-var plot_height_2 = svg_height - 2 * margin;
-// Create SVG for line plot
-var svg_2 = d3.select('body').append('svg')
-.attr('width', svg_width_2)
-.attr('height', svg_height);
-// x-axis label
-svg_2.append('text')
-  .attr('class', 'x-axis')
-  .attr('x', margin + plot_width / 2)
-  .atter('y', 3/2 * margin + plot_height)
-  .text('Time');
-// y-axis label
-svg_2.append('text')
-    .attr('class', 'y-axis')
-    .attr('text-anchor', 'middle')
-    .attr('transform',
-        'translate(' + margin/3 + ', ' + (plot_height / 2 + margin) + ')' +
-        'rotate(-90)')
-    .text('Number of Swipes');
-populateDayArray(trafficByFifteen, new Date("04/06/1997"), new Date("04/08/2019"));
-var max_avgin_listing = d3.max(dayData.AvgIn);
-var max_avgout_listing = d3.max(dayData.AvgOut);
-var xscale = d3.scaleTime().range([0, width])
-var yscale = d3.scaleLinear().
-}
-*/
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status Help
